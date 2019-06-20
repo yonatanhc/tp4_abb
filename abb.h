39665875e900b78d340c3e470fcf16b340304ac0
yonatanhc <39066273+yonatanhc@users.noreply.github.com>
2019-06-20 20:01:50 -0300

    Add files via upload

--- abb.h
+++ abb.h
@@ -3,35 +3,63 @@
 
 #include "nodo_abb.h"
 
-template <typename Dato>
 class Abb{
 	private:
 
-		Nodo_abb<Dato>* raiz;
+		Nodo_abb* raiz;
 		int cantidad;
 
 	public:
 		Abb();
 
-		void agregar_nodo(Dato d);
+		Nodo_abb* obtener_raiz();
 
-		void eliminar_nodo(Dato d);
+		void agregar_nodo(int clave,Cliente* d);
 
-		Nodo_abb<Dato>* buscar_nodo(Dato d);
+		bool eliminar_nodo(int clave);
+
+		Nodo_abb* buscar_nodo(int clave);
 
 		~Abb();
+
+	private:
+
+		Nodo_abb* buscar_padre(Nodo_abb* nodo,int clave);
+
+		void swap(Nodo_abb* nodo1,Nodo_abb* nodo2);
+
+		bool tipo_de_nodo(Nodo_abb* nodo,int clave);
+
+		bool eliminar_no_hoja(Nodo_abb* nodo,int clave);
+
+		Nodo_abb* buscar_izquierda_maximo(Nodo_abb* nodo);
+
+		bool eliminar_nodo_con_un_hijo(Nodo_abb* nodo,int clave);
+
+		Nodo_abb* buscar_nodo_derecho_minimo(Nodo_abb* nodo);
+
+		bool eliminar_nodo_con_dos_hijos(Nodo_abb* nodo);
+
+		void pos_orden(Nodo_abb* nodo);
+
+		Nodo_abb* buscar(Nodo_abb* nodo,int clave);
+
 	
 };
 
-template<typename Dato>
+
 Abb::Abb(){
 	raiz = NULL;
 	cantidad = 0;
 }
 
-Nodo_abb<Dato>* buscar_padre(Nodo_abb<Dato>* nodo,int clave){ //te fijas antes q el padre no sea raiz
+Nodo_abb* Abb::obtener_raiz(){
+	return raiz;
+}
+
+Nodo_abb* Abb::buscar_padre(Nodo_abb* nodo,int clave){ //te fijas antes q el padre no sea raiz
 	if(nodo->obtener_dato()->obtener_clave() > clave){
-		Nodo_abb<Dato>* nodo_izq = nodo->obtener_izquierda();
+		Nodo_abb* nodo_izq = nodo->obtener_izquierda();
 		if(nodo_izq){
 			if(nodo_izq->obtener_dato()->obtener_clave() == clave){
 				return nodo;
@@ -43,7 +71,7 @@ Nodo_abb<Dato>* buscar_padre(Nodo_abb<Dato>* nodo,int clave){ //te fijas antes q
 		else return nodo;
 	}
 	else{
-		Nodo_abb<Dato>* nodo_der = nodo->obtener_derecha();
+		Nodo_abb* nodo_der = nodo->obtener_derecha();
 		if(nodo_der){
 			if(nodo_der->obtener_dato()->obtener_clave() == clave){
 				return nodo;
@@ -57,7 +85,7 @@ Nodo_abb<Dato>* buscar_padre(Nodo_abb<Dato>* nodo,int clave){ //te fijas antes q
 }
 
 
-Nodo_abb<Dato>* buscar_nodo(Nodo_abb<Dato>* nodo,int clave){
+Nodo_abb* Abb::buscar(Nodo_abb* nodo,int clave){
 	if(nodo == NULL) return NULL;
 	if(nodo->obtener_dato()->obtener_clave() == clave){
 		return nodo;
@@ -69,24 +97,27 @@ Nodo_abb<Dato>* buscar_nodo(Nodo_abb<Dato>* nodo,int clave){
 	return buscar_nodo(nodo->obtener_derecha(),clave);
 }
 
+Nodo_abb* Abb::buscar_nodo(int clave){
+	return buscar(raiz,clave);
+}
 
 
-void Abb::agregar_nodo(Dato d){
-	Nodo_abb<Dato>* nuevo_nodo = new Nodo_abb(d);
+void Abb::agregar_nodo(int clave,Cliente* d){
+	Nodo_abb* nuevo_nodo = new Nodo_abb(clave,d);
 	if(cantidad == 0) {
 		raiz = nuevo_nodo;
 		cantidad++;
 	}
 	else{
-		Nodo_abb<Dato>* nodo = buscar_nodo(raiz,d->obtener_clave());
+		Nodo_abb* nodo = buscar_nodo(clave);
 		if(nodo) {
 			nodo->cambiar_dato(d);
 			delete(nuevo_nodo);
 		}
 		else{
 
-			Nodo_abb<Dato>* padre = buscar_padre(raiz,d->obtener_clave());
-			if(padre->obtener_dato()->obtener_clave() > d->obtener_clave()){
+			Nodo_abb* padre = buscar_padre(raiz,clave);
+			if(padre->obtener_dato()->obtener_clave() > clave){
 				padre->cambiar_izquierda(nodo);
 			}
 			else padre->cambiar_derecha(nodo);
@@ -95,13 +126,13 @@ void Abb::agregar_nodo(Dato d){
 	}
 }
 
-void Abb::swap(Nodo_abb<Dato>* nodo1,Nodo_abb<Dato>* nodo2){
-	Dato aux = nodo1->obtener_dato();
+void Abb::swap(Nodo_abb* nodo1,Nodo_abb* nodo2){
+	Cliente* aux = nodo1->obtener_dato();
 	nodo1->cambiar_dato(nodo2->obtener_dato());
 	nodo2->cambiar_dato(aux);
 }
 
-Nodo_abb<Nodo>* Abb::tipo_de_nodo(Nodo_abb<Dato>* nodo,int clave){
+bool Abb::tipo_de_nodo(Nodo_abb* nodo,int clave){
 	if(!nodo->obtener_derecha() && !nodo->obtener_izquierda()){
 		return eliminar_nodo_hoja(nodo,clave);
 	}
@@ -109,60 +140,75 @@ Nodo_abb<Nodo>* Abb::tipo_de_nodo(Nodo_abb<Dato>* nodo,int clave){
 		if(!nodo->obtener_derecha() || !nodo->obtener_izquierda()){
 			return eliminar_nodo_con_un_hijo(nodo,clave);
 		}
-		return eliminar_nodo_con_dos_hijos(nodo,clave);
+			return eliminar_nodo_con_dos_hijos(nodo,clave);
 	}
 }
 
-Nodo_abb<Dato>* Abb::eliminar_hoja(Nodo_abb<Dato>* nodo,int clave){
-	Nodo_abb<Dato>* padre = buscar_padre(raiz,clave);
-	if(padre->obtener_dato()->obtener_clave() > clave){
+bool Abb::eliminar_nodo_hoja(Nodo_abb* nodo,int clave){
+	Nodo_abb* padre = buscar_padre(raiz,clave);
+	if(padre->obtener_clave() > clave){
 		padre->cambiar_izquierda(NULL);
 	}
 	else padre->cambiar_derecha(NULL);
-	return nodo;
+	delete nodo;
+	cantidad--;
+	return true;
 }
 
-Nodo_abb<Dato>* Abb::buscar_izquierda_maximo(Nodo_abb<Dato>* nodo){
+Nodo_abb* Abb::buscar_izquierda_maximo(Nodo_abb* nodo){
 	if(!nodo->obtener_derecha()) return nodo;
 	return buscar_izquierda_maximo(nodo->obtener_derecha());
 }
 
-Nodo_abb<Dato>* Abb::eliminar_nodo_con_un_hijo(Nodo_abb<Dato>* nodo,int clave){
+bool  Abb::eliminar_nodo_con_un_hijo(Nodo_abb* nodo,int clave){
 	if(nodo->obtener_izquierda()){
-		Nodo_abb<Dato>* izq_max = buscar_izquierda_maximo(nodo->obtener_izquierda());
+		Nodo_abb* izq_max = buscar_izquierda_maximo(nodo->obtener_izquierda());
 		swap(nodo,izq_max);
 		return tipo_de_nodo(izq_max);
 	}
 	else{
-		Nodo_abb<Dato>* der_min = buscar_nodo_derecho_minimo(nodo->derecha());
+		Nodo_abb* der_min = buscar_nodo_derecho_minimo(nodo->derecha());
 		swap(nodo,der_min);
 		return tipo_de_nodo(der_min);
 	}
 }
 
-Nodo_abb<Dato>* Abb::buscar_nodo_derecho_minimo(Nodo_abb<Dato>* nodo){
+Nodo_abb* Abb::buscar_nodo_derecho_minimo(Nodo_abb* nodo){
 	if(nodo->obtener_izquierda() == NULL) return nodo;
 	else return buscar_nodo_derecho_minimo(nodo->obtener_izquierda());
 }
 
-Nodo_abb<Dato>* Abb::eliminar_nodo_con_dos_hijos(Nodo_abb<Dato>* nodo){
-	Nodo_abb<Dato>* aux = buscar_nodo_derecho_minimo(nodo->obtener_derecha());
+bool Abb::eliminar_nodo_con_dos_hijos(Nodo_abb* nodo){
+	Nodo_abb* aux = buscar_nodo_derecho_minimo(nodo->obtener_derecha());
 	swap(nodo,aux);
 	return tipo_de_nodo(aux);
 }
 
-Nodo_abb<Dato>* Abb::eliminar_nodo(int clave){
-	Nodo_abb<Dato>* nodo = buscar_nodo(raiz,clave);
-	if(!nodo) return NULL;
+bool Abb::eliminar_nodo(int clave){
+	Nodo_abb* nodo = buscar_nodo(clave);
+	if(!nodo) return false;
 	else{
 		if(cantidad == 1){
 			raiz = NULL;
-			return nodo;
+			delete nodo;
+			cantidad--;
+			return true;
 		}
 		else return tipo_de_nodo(nodo,clave);
 	}
 }
 
+void Abb::pos_orden(Nodo_abb* nodo){
+	if(!nodo) return;
+	pos_orden(nodo->obtener_izquierda());
+	pos_orden(nodo->obtener_derecha());
+	Nodo_abb* nodo = eliminar_hoja(nodo,nodo->obtener_clave());
+	delete nodo;
+	
+}
 
+Abb::~Abb(){
+	pos_orden(raiz);
+}
 
 #endif
--- nodo_abb.h
+++ nodo_abb.h
@@ -1,69 +1,74 @@
 #ifndef NODO_ABB_H
 #define NODO_ABB_H
 
-template <typename Dato>
+#include "cliente.h"
+
 class Nodo_abb{
 
 	private:
-			
-		Dato dato;
-		Nodo_abb<Dato>* derecha;
-		Nodo_abb>Dato>* izquierda;
+		int clave;	
+		Cliente* dato;
+		Nodo_abb* derecha;
+		Nodo_abb* izquierda;
 
 	public:
 
-		Nodo_abb(Dato d);
+		Nodo_abb(int clave,Cliente* d);
 
-		void cambiar_dato(Dato d);
+		void cambiar_dato(Cliente* d);
 
-		void cambiar_derecha(Nodo_abb<Dato>* der);
+		void cambiar_derecha(Nodo_abb* der);
 
-		void cambiar_izquierda(Nodo_abb<Dato>* izq);
+		void cambiar_izquierda(Nodo_abb* izq);
 
-		Dato obtener_dato();
+		Cliente* obtener_dato();
 
-		Nodo_abb<Dato>* obtener_derecha();
+		int clave obtener_clave();
 
-		Nodo_abb<Dato>* obtener_izquierda();
+		Nodo_abb* obtener_derecha();
+
+		Nodo_abb* obtener_izquierda();
 
 		~Nodo_abb();
 	
 };
 
-template<typename Dato>
-Nodo_abb<Dato>::Nodo_abb(Dato d){
+
+Nodo_abb::Nodo_abb(int clave,Cliente* d){
+	this->clave = clave;
 	dato = d;
 	derecha = NULL;
 	izquierda = NULL;
 }
 
-template<typename Dato>
-void Nodo_abb<Dato>::cambiar_dato(Dato d){
+void Nodo_abb::cambiar_dato(Cliente* d){
 	dato = d;
 }
 
-template<typename Dato>
-void Nodo_abb<Dato>::cambiar_derecha(Nodo_abb<Dato>* der){
+
+void Nodo_abb::cambiar_derecha(Nodo_abb* der){
 	derecha = der;
 }
 
-template<typename Dato>
-void Nodo_abb<Dato>::cambiar_izquierda(Nodo_abb<Dato>* izq){
+
+void Nodo_abb::cambiar_izquierda(Nodo_abb* izq){
 	izquierda = izq;
 }
 
-template<typename Dato>
-Dato Nodo_abb<Dato>::obtener_dato(){
+
+Cliente* Nodo_abb::obtener_dato(){
 	return dato;
 }
 
-template<typename Dato>
-Nodo_abb<Dato>* Nodo_abb<Dato>::obtener_derecha(){
+int Nodo_abb::obtener_clave(){
+	return clave;
+}
+
+Nodo_abb* Nodo_abb::obtener_derecha(){
 	return derecha;
 }
 
-template<typename Dato>
-Nodo_abb<Dato>* Nodo_abb<Dato>::obtener_izquierda(){
+Nodo_abb* Nodo_abb::obtener_izquierda(){
 	return izquierda;
 }
 
